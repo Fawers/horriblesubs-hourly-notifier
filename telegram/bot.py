71377ed377fa148e6da8b5cf99b6d72ed2d3d921
Fawers <fabricioswerneck@gmail.com>
2018-07-08 02:57:18 -0300

    add telegram bot support

---
+++ telegram/bot.py
@@ -0,0 +1,12 @@
+import os
+
+import telepot
+
+
+CHANNEL = os.getenv('TGCHANNEL')
+
+BOT = telepot.Bot(os.getenv('TGBOT'))
+
+
+def send_to_channel(message):
+    BOT.sendMessage(CHANNEL, message, parse_mode='markdown')
